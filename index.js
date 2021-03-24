4245e288d77ee7da6f88b4612944ffe706832251
Kristiyan Raykov <craykov@yahoo.com>
2021-03-24 20:16:02 +0200

    implemented router

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const handlebars = require('express-handlebars')
-const config = require('./config/config')
+const routes = require('./routes')
 const app = express();
 
-app.engine('hbs', handlebars({
-    extname: 'hbs'
-}))
-app.set('view engine', 'hbs');
+const config = require('./config/config')
+const expressConfig = require('./config/express')
+expressConfig(app);
 
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home', {layout: false})
-})
+app.use(routes);
+
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
