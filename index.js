df1af3397ed083a907d653511f65002a8239edcd
Kristiyan Raykov <craykov@yahoo.com>
2021-03-24 18:53:46 +0200

    initialized express and view engine

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const handlebars = require('express-handlebars')
+const config = require('./config/config')
+const app = express();
+
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', {layout: false})
+})
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
