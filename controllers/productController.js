b27d9ac0bde61ed1637e733cde6f6a55e2b1daad
Kristiyan Raykov <craykov@yahoo.com>
2021-03-25 16:15:51 +0200

    added details page

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,5 +10,8 @@ router.get('/create', (req, res) => {
     res.render('create', {title: 'Create'})
 });
 
+router.get('/details/:productId', (req, res) => {
+    res.render('details')
+})
 
 module.exports = router;
