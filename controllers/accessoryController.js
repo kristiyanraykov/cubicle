12bb9cf0016a36fe1dd85e398e36a3aef857c4ea
Kristiyan Raykov <craykov@yahoo.com>
2021-03-29 17:08:05 +0300

    added accossories service and controller

--- controllers/accessoryController.js
+++ controllers/accessoryController.js
@@ -1,2 +1,16 @@
 const { Router } = require('express');
 
+const accessoryService = require ('../services/accessoryService')
+
+const router = Router();
+
+router.get('/create', (req, res) => {
+    res.render('createAccessory')
+})
+
+router.post('/create', (req, res) => {
+    accessoryService.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(() => res.status(500).end())
+})
+module.exports = router;
--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const productController = require('./controllers/productController');
 const aboutController = require('./controllers/aboutController');
+const accessoryControler = require('./controllers/accessoryController')
 
 const router = Router();
 
 router.use('/', productController);
 router.use('/about', aboutController);
+router.use('/accessories', accessoryControler);
 router.get('*', (req, res) => {
     res.render('404')
 })
